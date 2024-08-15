a8b5c72fc4dcb6bb45551d7d7a3f86c796bb5486
IhorB21 <igorbereznij90@gmail.com>
2024-08-15 20:31:42 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import random
 
-flag = True
+
+def main():
+    flag = True
+
+    l_side = int(input('Enter teh left side: '))
+    r_side = int(input('Enter teh right side: '))
+    attempts = int(input('Enter attempts count: '))
+
+    if attempts <= 0:
+        print('Input another attempts count!!!')
+        return
+
+    if l_side >= r_side:
+        print('Input another numbers!!!')
+        return
+
+    random_number = random.randint(l_side, r_side+1)
+    print(random_number)
+
+    for i in range(1, attempts+1):
+        print(f'Its {i} attempts')
+        try:
+            answer = int(input('Enter your number: '))
+        except TypeError:
+            print('Input number')
+            continue
+
+        if answer == random_number:
+            print('You won!!!')
+            return
+
+        else:
+            if i == attempts:
+                print('It was your last attempt!!!')
+                return
+            print('Try again')
 
 
-l_side = int(input('Enter teh left side: '))
-r_side = int(input('Enter teh right side: '))
-
-
-if l_side >= r_side:
-    print('Input another numbers!!!')
-    flag = False
-
-random_number = random.randint(l_side, r_side+1)
-print(random_number)
-
-while flag:
-    answer = int(input('Enter your number: '))
-    if answer == random_number:
-        print('You won!!!')
-        flag = False
-
-    else:
-        print('Try again')
+if __name__ == '__main__':
+    main()
