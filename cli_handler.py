ab0248cd60a2522e41d232ef57e94774a13f957f
Nik-BZ-22 <bakhamyt@gmail.com>
2024-08-16 13:33:18 +0300

    Changed the logic of logging a bit. Added CLI interface

---
+++ cli_handler.py
@@ -0,0 +1,11 @@
+
+import argparse
+
+def get_cli_args() -> dict:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-l", "--left_bound", type=int, help="Left bound of range. Default=0 ", default=0)
+    parser.add_argument("-r", "--right_bound", type=int, help="Right bound of range. Default=1", default=1)
+    parser.add_argument("-a", "--attempts_count", type=int, help="Number of attempts. Default=3", default=3)
+
+    args = parser.parse_args().__dict__
+    return args
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import random
 class GuessTheNumberGame:
-
     def __init__(self, attempts_count=1, left_bound=0, right_bound=1):
-        self.left_bound = left_bound
-        self.right_bound = right_bound
-        self.attempts_count = attempts_count
-        self.target_number = None
+        self.left_bound      = left_bound if left_bound < right_bound else 0
+        self.right_bound     = right_bound if right_bound > left_bound else 1
+        self.attempts_count  = attempts_count if attempts_count > 0 else 1
+        self.target_number   = None
+        self.message         = f'[{self.left_bound} : {self.right_bound}]\nEnter your number: '
+
 
     def __str__(self):
         return f"[{self.left_bound} : {self.right_bound}] ({self.attempts_count})"
@@ -32,9 +33,10 @@ class GuessTheNumberGame:
         self.set_and_get_num()
         print(self.target_number)
         for atmp in range(1, self.attempts_count+1):
-            print(f"Your attempt #{atmp}")
+            rest = self.attempts_count-atmp
+            print(f"Your attempt #{atmp} ({rest} more left)")
 
-            while (answer := input('Enter your number: ')) and (not answer.isdigit()):
+            while (answer := input(self.message).strip()) and (not answer.lstrip('-').isdigit()) or (len(answer) <= 0):
                 print(f"<{answer}>", "[It isn't a number]")
             else:
                 answer = int(answer)
@@ -47,9 +49,15 @@ class GuessTheNumberGame:
                 print("YOU WIN")
                 return
 
-            print("TRY AGAIN\n", "_"*20, sep="")
+            if rest != 0:
+                print("TRY AGAIN\n", "_"*30, sep="")
+            else:
+                print("This was your last attempt. Bye")
 
 
 
-game = GuessTheNumberGame(attempts_count=3, left_bound=5, right_bound=20)
-game.play()
+if __name__ == "__main__":
+    import cli_handler
+    args = cli_handler.get_cli_args()
+    game = GuessTheNumberGame(**args)
+    game.play()
